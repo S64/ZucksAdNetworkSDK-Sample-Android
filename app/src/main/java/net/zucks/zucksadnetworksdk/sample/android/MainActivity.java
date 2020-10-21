37611bbd0cbe78828b0f63c854a3e04b0ce500c7
Shuma Yoshioka <shuma_yoshioka@voyagegroup.com>
2020-10-21 12:15:48 +0900

    Link from launcher

--- app/src/main/java/net/zucks/zucksadnetworksdk/sample/android/MainActivity.java
+++ app/src/main/java/net/zucks/zucksadnetworksdk/sample/android/MainActivity.java
@@ -76,5 +76,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button rewardedAdButton = findViewById(R.id.button_rewarded_ad);
+        rewardedAdButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RewardedActivity.class));
+            }
+
+        });
+
     }
 }
